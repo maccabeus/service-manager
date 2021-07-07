22b8b868e9be0a9d11d4d48ff963f113e9b0b789
maccaabeus <mlisoftinc@gmail.coms>
2021-07-07 16:24:53 +0100

    Fix: Issue with gunicorn server crashing

--- servicemanager/asgi.py
+++ servicemanager/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for macarysserver project.
+ASGI config for servicemanager project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'macarysserver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'servicemanager.settings')
 
 application = get_asgi_application()
